93cd388f997065a1cffeafa267896f26c937af88
Tomonori Tanabe <tanb+github@me.com>
2013-09-18 02:35:32 +0900

    Refactored.

--- TBBackdropView/TBBackdropView.m
+++ TBBackdropView/TBBackdropView.m
@@ -10,6 +10,41 @@
 
 @implementation TBBackdropView
 
+- (id)initWithFrame:(CGRect)frame settingsType:(TBBackdropViewSettingsType)settingsType
+{
+    self = [super initWithFrame:frame];
+    if (!self) return nil;
+    self.backgroundColor = [UIColor clearColor];
+    
+    Class klass = NSClassFromString(@"_UIBackdropView");
+    _backdropView = [klass alloc];
+    
+    CGRect bounds = self.bounds;
+    id settings = [self _generateBackdropViewSettingsForSettingsType:settingsType];
+    
+    SEL sel = NSSelectorFromString(@"initWithFrame:settings:");
+    NSMethodSignature *msig = [_backdropView methodSignatureForSelector:sel];
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:msig];
+    [inv setSelector:sel];
+    [inv setTarget:_backdropView];
+    [inv setArgument:&bounds atIndex:2];
+    [inv setArgument:&settings atIndex:3];
+    [inv invoke];
+    
+    [self addSubview:_backdropView];
+    
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [self initWithFrame:frame settingsType:TBBackdropViewSettingsBlur];
+    if (!self) return nil;
+
+    return self;
+}
+
+
 - (id)_generateBackdropViewSettingsForSettingsType:(TBBackdropViewSettingsType)settings
 {
     id backdropViewSettings = nil;
@@ -91,107 +126,6 @@
     }
     
     return backdropViewSettings;
-}
-
-- (id)initWithFrame:(CGRect)frame settingsType:(TBBackdropViewSettingsType)settingsType
-{
-    self = [super initWithFrame:frame];
-    if (!self) return nil;
-    self.backgroundColor = [UIColor clearColor];
-    
-    Class klass = NSClassFromString(@"_UIBackdropView");
-    _backdropView = [klass alloc];
-    
-    CGRect bounds = self.bounds;
-    id settings = [self _generateBackdropViewSettingsForSettingsType:settingsType];
-    
-    SEL sel = NSSelectorFromString(@"initWithFrame:settings:");
-    NSMethodSignature *msig = [_backdropView methodSignatureForSelector:sel];
-    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:msig];
-    [inv setSelector:sel];
-    [inv setTarget:_backdropView];
-    [inv setArgument:&bounds atIndex:2];
-    [inv setArgument:&settings atIndex:3];
-    [inv invoke];
-    
-    [self addSubview:_backdropView];
-    
-    return self;
-}
-
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (!self) return nil;
-    self.backgroundColor = [UIColor clearColor];
-    
-    Class klass = NSClassFromString(@"_UIBackdropView");
-    _backdropView = [klass alloc];
-    CGRect bounds = self.bounds;
-    SEL sel = NSSelectorFromString(@"initWithFrame:");
-    NSMethodSignature *msig = [_backdropView methodSignatureForSelector:sel];
-    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:msig];
-    [inv setSelector:sel];
-    [inv setTarget:_backdropView];
-    [inv setArgument:&bounds atIndex:2];
-    [inv invoke];
-    [self addSubview:_backdropView];
-
-    return self;
-}
-
-- (id)gaussianBlurFilter
-{
-    id gaussianBlurFilter = [_backdropView performSelector:@selector(gaussianBlurFilter)];
-    NSLog(@"%@",[gaussianBlurFilter valueForKey:@"inputRadius"]);
-    NSLog(@"%@", [gaussianBlurFilter valueForKey:@"inputHardEdges"]);
-    return gaussianBlurFilter;
-}
-
-- (void)setBlurFilterWithRadius:(float)radius blurQuality:(id)quality blurHardEdges:(int)edges
-{
-    SEL sel = NSSelectorFromString(@"setBlurFilterWithRadius:blurQuality:blurHardEdges:");
-    NSMethodSignature *msig = [_backdropView methodSignatureForSelector:sel];
-    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:msig];
-    [inv setSelector:sel];
-    [inv setTarget:_backdropView];
-    [inv setArgument:&radius atIndex:2];
-    [inv setArgument:&quality atIndex:3];
-    [inv setArgument:&edges atIndex:4];
-    [inv invoke];
-}
-
-- (void)setColorMatrixColorTintAlpha:(float)alpha
-{
-    SEL sel = NSSelectorFromString(@"setColorMatrixColorTintAlpha:");
-    NSMethodSignature *msig = [_backdropView methodSignatureForSelector:sel];
-    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:msig];
-    [inv setSelector:sel];
-    [inv setTarget:_backdropView];
-    [inv setArgument:&alpha atIndex:2];
-    [inv invoke];
-}
-
-- (void)setColorMatrixGrayscaleTintAlpha:(float)alpha
-{
-    SEL sel = NSSelectorFromString(@"setColorMatrixGrayscaleTintAlpha:");
-    NSMethodSignature *msig = [_backdropView methodSignatureForSelector:sel];
-    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:msig];
-    [inv setSelector:sel];
-    [inv setTarget:_backdropView];
-    [inv setArgument:&alpha atIndex:2];
-    [inv invoke];
-}
-
-- (void)setBlursBackground:(BOOL)blur
-{
-    SEL sel = NSSelectorFromString(@"setBlursBackground:");
-    NSMethodSignature *msig = [_backdropView methodSignatureForSelector:sel];
-    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:msig];
-    [inv setSelector:sel];
-    [inv setTarget:_backdropView];
-    [inv setArgument:&blur atIndex:2];
-    [inv invoke];
 }
 
 @end
